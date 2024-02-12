445d2e630846a055c6654498298314ba3f88f4ee
Rajneesh kumar <rajnishkumar550443@gmail.com>
2024-02-12 23:23:27 +0530

    ternary operator

---
+++ ternaryOp/odd.java
@@ -0,0 +1,11 @@
+package ternaryOp;
+
+public class odd {
+    public static void main(String[] args) {
+        int num = 5;
+    //ternary operator
+        String chec = ((num%2)==0) ? "even":"odd";
+        System.out.print(chec);
+    }
+    
+}
